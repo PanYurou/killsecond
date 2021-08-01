c0b6420422158b90fb2cc8b71742abf71c909b32
yuroupan <yrpan@jinshuju.net>
2021-08-01 20:48:27 +0800

    First commit

---
+++ src/main/java/com/pyr/Application.java
@@ -0,0 +1,17 @@
+package com.pyr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author yuroupan
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
